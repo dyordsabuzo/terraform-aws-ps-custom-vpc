b15358912eee4a8606b984b21fbed8e2a04ae82c
dyordsabuzo <dyordsabuzo@gmail.com>
2021-08-14 11:59:39 +1000

    Create private subnet resources (#1)

--- locals.tf
+++ locals.tf
@@ -7,4 +7,11 @@ locals {
     0,
     var.availability_zone_count
   )
+
+  nat_gateway_count = var.single_nat_gateway ? 1 : var.availability_zone_count
+  nat_azs           = slice(local.azs, 0, local.nat_gateway_count)
+
+  public_subnet_ids = [for key, public_subnet in aws_subnet.public_subnet : public_subnet.id]
+
+  private_route_table_ids = [for key, route_table in aws_route_table.private_route_table : route_table.id]
 }
