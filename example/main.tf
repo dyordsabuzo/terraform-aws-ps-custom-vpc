e1aac62c86426e5d274a5b76a5091975f4245d9c
Dyords <dyordsabuzo@gmaail.com>
2021-09-23 20:40:24 +1000

    Make resource identifier as variable

--- example/main.tf
+++ example/main.tf
@@ -2,7 +2,7 @@ module "my_vpc" {
   source                  = "../"
   vpc_cidr_block          = local.vpc_cidr
   public_subnets          = local.public_subnets
-  resource_identifier     = "ps-custom"
+  resource_identifier     = var.resource_identifier
   availability_zone_count = local.az_count
   private_subnets         = local.private_subnets
   tags                    = local.tags
--- example/variables.tf
+++ example/variables.tf
@@ -3,3 +3,8 @@ variable "region" {
   description = "AWS region where resources will be created in"
   default     = "ap-southeast-2"
 }
+
+variable "resource_identifier" {
+  type = string
+  description = "Resource identifier"
+}
