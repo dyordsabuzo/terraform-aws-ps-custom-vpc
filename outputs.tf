0ab5261580e7d99fbce1e1187f9e6c6a156ddb21
Dyords Abuzo <dyordsabuzo@gmail.com>
2026-01-11 09:55:53 +1100

    Add subnets on the output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
 output "vpc_id" {
+  description = "The ID of the VPC"
   value = aws_vpc.vpc.id
+}
+
+output "public_subnets" {
+  description = "The IDs of the public subnets"
+  value = aws_subnet.public.*.id
+}
+
+output "private_subnets" {
+  description = "The IDs of the private subnets"
+  value = aws_subnet.private.*.id
 }
