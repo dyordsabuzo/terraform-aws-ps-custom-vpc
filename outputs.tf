0ca8da248406a3cac863b7d505633588efea3056
Dyords Abuzo <dyordsabuzo@gmail.com>
2026-01-11 16:38:43 +1100

    Change formatting of returned subnet ids

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value = aws_vpc.vpc.id
+  value       = aws_vpc.vpc.id
 }
 
 output "public_subnets" {
   description = "The IDs of the public subnets"
-  value = aws_subnet.public_subnet.*.id
+  value       = values(aws_subnet.public_subnet)[*].id
 }
 
 output "private_subnets" {
   description = "The IDs of the private subnets"
-  value = aws_subnet.private_subnet.*.id
+  value       = values(aws_subnet.private_subnet)[*].id
 }
